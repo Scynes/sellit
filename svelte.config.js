1e70e3166c17111b41f2f3ebc229681ce71b9f7e
Scynes <dustin.greyfield@gmail.com>
2023-02-14 16:25:42 -0500

    complete: added supabase util module

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
+import dotenv from 'dotenv';
+import { instantiate, getSupabaseConnection } from './src/utils/supabase.js'
+
+dotenv.config();
+
+instantiate(process.env.DATABASE_URL, process.env.DATABASE_SECRET_KEY);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
